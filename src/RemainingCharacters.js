0f0b17e801c41896ebf4729b800fe2319defb44c
David Tang <david.tang@verizondigitalmedia.com>
2019-09-19 20:24:36 -0700

    propTypes

--- src/RemainingCharacters.js
+++ src/RemainingCharacters.js
@@ -1,7 +1,13 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export default function RemainingCharacters(props) {
   const { max, text, children } = props;
   const remaining = max - text.length;
   return <>{children ? children(remaining) : remaining}</>;
 }
+
+RemainingCharacters.propTypes = {
+  text: PropTypes.string.isRequired,
+  max: PropTypes.number.isRequired
+};

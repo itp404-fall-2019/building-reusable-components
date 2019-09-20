f6e4c006d63cd0d0e9e97ad544a4823801e115ea
David Tang <david.tang@verizondigitalmedia.com>
2019-09-19 22:05:03 -0700

    propTypes

--- src/InlineEdit.js
+++ src/InlineEdit.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const ESCAPE_KEY = 27;
 const ENTER_KEY = 13;
@@ -56,3 +57,8 @@ export default class InlineEdit extends React.Component {
     );
   }
 }
+
+InlineEdit.propTypes = {
+  value: PropTypes.string.isRequired,
+  onEnter: PropTypes.func.isRequired
+};

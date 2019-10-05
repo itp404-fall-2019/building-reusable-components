646e54d12d798333f73ea23069ef7346ef17b2af
David Tang <dtang@auditboard.com>
2019-10-05 09:52:24 -0700

    test InlineEdit

---
+++ src/InlineEdit.test.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import InlineEdit from "./InlineEdit";
+import "@testing-library/jest-dom/extend-expect";
+
+it("renders the value as text", () => {
+  const { container } = render(
+    <InlineEdit value={"Untitled Document"} onEnter={() => {}} />
+  );
+
+  expect(container.textContent).toBe("Untitled Document");
+});
+
+it("changes to an input when clicked", () => {
+  let title = "Untitled Document";
+  const { container, getByText } = render(
+    <InlineEdit value={title} onEnter={() => {}} />
+  );
+  fireEvent.click(getByText("Untitled Document"));
+  expect(container.querySelector("input").value).toBe("Untitled Document");
+  // expect(container.querySelector("input")).toHaveValue("Untitled Document");
+});
+
+it("updates to the new value when enter is pressed", () => {
+  let title = "Untitled Document";
+  let updateTitle = newTitle => (title = newTitle);
+
+  const { container, getByText } = render(
+    <InlineEdit value={title} onEnter={updateTitle} />
+  );
+
+  fireEvent.click(getByText("Untitled Document"));
+  const input = container.querySelector("input");
+
+  fireEvent.change(input, {
+    target: {
+      value: "USC"
+    }
+  });
+
+  fireEvent.keyUp(input, {
+    keyCode: 13
+  });
+
+  expect(title).toBe("USC");
+});
+
+it("does not update to the new value when escape is pressed", () => {
+  let title = "Untitled Document";
+  let updateTitle = newTitle => (title = newTitle);
+  const { container, getByText } = render(
+    <InlineEdit value={title} onEnter={updateTitle} />
+  );
+
+  fireEvent.click(getByText("Untitled Document"));
+  const input = container.querySelector("input");
+
+  fireEvent.change(input, {
+    target: {
+      value: "USC"
+    }
+  });
+
+  fireEvent.keyUp(input, {
+    keyCode: 27
+  });
+
+  expect(title).toBe("Untitled Document");
+});
